b39b53594850e25693204e359a460a73d9215457
Franco Biasin <franconbiasin@gmail.com>
2021-03-30 11:23:08 -0300

    Modify compiler so it can use custom components

--- src/reagent/impl/template.cljs
+++ src/reagent/impl/template.cljs
@@ -10,16 +10,13 @@
             [reagent.debug :refer-macros [dev? warn]]
             [goog.object :as gobj]))
 
-;; From Weavejester's Hiccup, via pump:
-(def ^{:doc "Regular expression that parses a CSS-style id and class
-             from a tag name."}
-  re-tag #"([^\s\.#]+)(?:#([^\s\.#]+))?(?:\.([^\s#]+))?")
+(defonce components (atom {}))
 
-(deftype NativeWrapper [tag id className])
+(deftype NativeWrapper [component])
 
 (defn adapt-react-class
   [c]
-  (->NativeWrapper c nil nil))
+  (->NativeWrapper c))
 
 ;;; Common utilities
 
@@ -34,12 +31,7 @@
 
 ;;; Props conversion
 
-;; TODO: Move prop-name caches to the compiler object, if this
-;; conversion can be configured.
-
-(def prop-name-cache #js{:class "className"
-                         :for "htmlFor"
-                         :charset "charSet"})
+(def prop-name-cache #js{})
 
 (defn cache-get [o k]
   (when ^boolean (.hasOwnProperty o k)
@@ -69,61 +61,6 @@
                    (apply x args))
         :else (clj->js x)))
 
-;; Previous few functions copied for custom elements,
-;; without mapping from class to className etc.
-
-(def custom-prop-name-cache #js{})
-
-(defn cached-custom-prop-name [k]
-  (if (named? k)
-    (if-some [k' (cache-get custom-prop-name-cache (name k))]
-      k'
-      (let [v (util/dash-to-prop-name k)]
-        (gobj/set custom-prop-name-cache (name k) v)
-        v))
-    k))
-
-(defn custom-kv-conv [o k v]
-  (doto o
-    (gobj/set (cached-custom-prop-name k) (convert-prop-value v))))
-
-(defn convert-custom-prop-value [x]
-  (cond (util/js-val? x) x
-        (named? x) (name x)
-        (map? x) (reduce-kv custom-kv-conv #js{} x)
-        (coll? x) (clj->js x)
-        (ifn? x) (fn [& args]
-                   (apply x args))
-        :else (clj->js x)))
-
-(defn set-id-class
-  "Takes the id and class from tag keyword, and adds them to the
-  other props. Parsed tag is JS object with :id and :class properties."
-  [props id-class]
-  (let [id (.-id id-class)
-        class (.-className id-class)]
-    (cond-> props
-      ;; Only use ID from tag keyword if no :id in props already
-      (and (some? id)
-           (nil? (:id props)))
-      (assoc :id id)
-
-      ;; Merge classes
-      class
-      ;; Note: someone might use React-style :className property,
-      ;; this is the only place where that needs special case. Using
-      ;; :class and :className together is not supported.
-      (assoc :class (util/class-names class (or (:class props) (:className props)))))))
-
-(defn convert-props [props ^clj id-class]
-  (let [class (:class props)
-        props (-> props
-                  (cond-> class (assoc :class (util/class-names class)))
-                  (set-id-class id-class))]
-    (if (.-custom id-class)
-      (convert-custom-prop-value props)
-      (convert-prop-value props))))
-
 ;;; Conversion from Hiccup forms
 
 (defn make-element [this argv component jsprops first-child]
@@ -140,20 +77,6 @@
                           (.push a (p/as-element this v)))
                          a)
                        #js [component jsprops] argv))))
-
-(deftype HiccupTag [tag id className custom])
-
-(defn parse-tag [hiccup-tag]
-  (let [[tag id className] (->> hiccup-tag name (re-matches re-tag) next)
-        className (when-not (nil? className)
-                    (string/replace className #"\." " "))]
-    (assert tag (str "Invalid tag: '" hiccup-tag "'" (comp/comp-name)))
-    (->HiccupTag tag
-                 id
-                 className
-                 ;; Custom element names must contain hyphen
-                 ;; https://www.w3.org/TR/custom-elements/#custom-elements-core-concepts
-                 (not= -1 (.indexOf tag "-")))))
 
 (defn reag-element [tag v compiler]
   (let [c (comp/as-class tag compiler)
@@ -190,23 +113,13 @@
       (set! (.-key jsprops) key))
     (p/make-element compiler argv react/Fragment jsprops first-child)))
 
-(def tag-name-cache #js {})
-
-(defn cached-parse [x]
-  (if-some [s (cache-get tag-name-cache x)]
-    s
-    (let [v (parse-tag x)]
-      (gobj/set tag-name-cache x v)
-      v)))
-
-(defn native-element [parsed argv first ^p/Compiler compiler]
-  (let [component (.-tag parsed)
-        props (nth argv first nil)
+(defn native-element [component argv first ^p/Compiler compiler]
+  (let [props (nth argv first nil)
         hasprops (or (nil? props) (map? props))
-        jsprops (or (convert-props (if hasprops props) parsed)
+        jsprops (or (convert-prop-value (if hasprops props))
                     #js {})
         first-child (+ first (if hasprops 1 0))]
-    (if (input/input-component? component)
+    (if (and (object? component) (gobj/get component "isInput"))
       (let [input-class (or (.-reagentInput compiler)
                             (let [x (comp/create-class input/input-spec compiler)]
                               (set! (.-reagentInput compiler) x)
@@ -247,22 +160,6 @@
       (warn (util/hiccup-err x (comp/comp-name) "Every element in a seq should have a unique :key")))
     res))
 
-(defn hiccup-element [v compiler]
-  (let [tag (nth v 0 nil)
-        n (name tag)
-        pos (.indexOf n ">")]
-    (case pos
-      -1 (native-element (cached-parse n) v 1 compiler)
-      0 (assert (= ">" n) (util/hiccup-err v (comp/comp-name) "Invalid Hiccup tag"))
-      ;; Support extended hiccup syntax, i.e :div.bar>a.foo
-      ;; Apply metadata (e.g. :key) to the outermost element.
-      ;; Metadata is probably used only with sequeneces, and in that case
-      ;; only the key of the outermost element matters.
-      (recur (with-meta [(subs n 0 pos)
-                         (assoc (with-meta v nil) 0 (subs n (inc pos)))]
-                        (meta v))
-             compiler))))
-
 (defn vec-to-elem [v compiler fn-to-element]
   (when (nil? compiler)
     (js/console.error "vec-to-elem" (pr-str v)))
@@ -270,18 +167,18 @@
   (let [tag (nth v 0 nil)]
     (assert (valid-tag? tag) (util/hiccup-err v (comp/comp-name) "Invalid Hiccup form"))
     (case tag
-      :> (native-element (->HiccupTag (nth v 1 nil) nil nil nil) v 2 compiler)
+      :> (native-element (nth v 1 nil) v 2 compiler)
       :r> (raw-element (nth v 1 nil) v compiler)
       :f> (function-element (nth v 1 nil) v 2 compiler)
       :<> (fragment-element v compiler)
       (cond
-       (hiccup-tag? tag)
-       (hiccup-element v compiler)
+        (get @components tag)
+        (native-element (get @components tag) v 1 compiler)
 
-       (instance? NativeWrapper tag)
-       (native-element tag v 1 compiler)
+        (instance? NativeWrapper tag)
+        (native-element (.-component tag) v 1 compiler)
 
-       :else (fn-to-element tag v compiler)))))
+        :else (fn-to-element tag v compiler)))))
 
 (defn as-element [this x fn-to-element]
   (cond (util/js-val? x) x
@@ -298,6 +195,8 @@
         fn-to-element (if (:function-components opts)
                         maybe-function-element
                         reag-element)]
+    (when-let [c (:components opts)]
+      (reset! components c))
     (reify p/Compiler
       ;; This is used to as cache key to cache component fns per compiler
       (get-id [this] id)
@@ -307,6 +206,7 @@
         (make-element this argv component jsprops first-child)))))
 
 (def default-compiler* (create-compiler {}))
+
 (def ^:dynamic default-compiler default-compiler*)
 
 (defn set-default-compiler! [compiler]
